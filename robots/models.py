b1ec09012f5292df50a3078d596a3e729394f1ad
unknown <dontparryme@ymail.com>
2023-10-03 16:29:42 +0300

    remove requirements, remove unused dependencies

--- robots/models.py
+++ robots/models.py
@@ -1,8 +1,11 @@
+import uuid
 from django.db import models
+from django.utils import timezone
 
 
 class Robot(models.Model):
-    serial = models.CharField(max_length=5, blank=False, null=False)
+    serial = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     model = models.CharField(max_length=2, blank=False, null=False)
     version = models.CharField(max_length=2, blank=False, null=False)
-    created = models.DateTimeField(blank=False, null=False)
+    created = models.DateTimeField(default=timezone.now, blank=False, null=False)
+
