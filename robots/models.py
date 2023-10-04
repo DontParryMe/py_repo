9baef5e9306a294d0e40d2bd01f80176065285a5
unknown <dontparryme@ymail.com>
2023-10-04 13:42:29 +0300

    test done

--- robots/models.py
+++ robots/models.py
@@ -1,8 +1,12 @@
+import uuid
+from django.utils import timezone
 from django.db import models
 
 
 class Robot(models.Model):
-    serial = models.CharField(max_length=5, blank=False, null=False)
+    serial = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     model = models.CharField(max_length=2, blank=False, null=False)
     version = models.CharField(max_length=2, blank=False, null=False)
-    created = models.DateTimeField(blank=False, null=False)
+    created = models.DateTimeField(default=timezone.now, blank=False, null=False)
+
+
